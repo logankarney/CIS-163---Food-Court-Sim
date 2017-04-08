8d9a31a5697350a0079001164ef6257ce535ae9a
logankarney <karneyl@mail.gvsu.edu>
2017-04-08 16:12:55 -0400

    Update PersonProducer
    added specialization to the People added to the eatery

--- src/backEnd/PersonProducer.java
+++ src/backEnd/PersonProducer.java
@@ -30,16 +30,29 @@ public class PersonProducer implements ClockListener {
 	public void event(int tick) {
 		if (nextPerson <= tick) {
 			nextPerson = tick + numOfTicksNextPerson;
-			
-			Person person = new Person();
-			
-			int rNumber = (int)(Math.random() * 100);
 
-			person.setEateryTime(averageEateryTime*0.5*r.nextGaussian() + averageEateryTime +.5);
+			Person person = new Person();
+
+			int rNumber = (int) (Math.random() * 100);
+
+			person.setEateryTime(averageEateryTime * 0.5 * r.nextGaussian() + averageEateryTime + .5);
 			person.setTickTime(tick);
-			eatery.add(person);
-			
-		//	person.setDestination(theLocationAfterTheEatery);  // You can save off where the person should go.
+
+			// Determines what type of Person the customer will be
+			int rType = (int) (Math.random() * 100);
+
+			Person customer = person;
+			if (rType < 70)
+				customer = new RegularPerson();
+			else if(rType < 90)
+				customer = new LimitedTimePerson();
+			else
+				customer = new RegularPerson();
+
+			eatery.add(customer);
+
+			// person.setDestination(theLocationAfterTheEatery); // You can save
+			// off where the person should go.
 		}
 	}
 
