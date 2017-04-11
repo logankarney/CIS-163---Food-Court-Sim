066ce13f21e80028fbdfef5f18532fa283c60a73
logankarney <karneyl@mail.gvsu.edu>
2017-04-11 18:22:33 -0400

    added type Enum

--- src/backEnd/LimitedTimePerson.java
+++ src/backEnd/LimitedTimePerson.java
@@ -4,10 +4,11 @@ public class LimitedTimePerson extends Person{
 
 		public LimitedTimePerson(){
 			super();
+			this.type = TypeOfPerson.LIMITED_TIME;
     }
 	public LimitedTimePerson(Person p){
 			super(p);
-			
+			this.type = TypeOfPerson.LIMITED_TIME;
 			setTickTime((int)(getTickTime() * 0.5));
 			setEateryTime(getEateryTime() * .5);
 		}
