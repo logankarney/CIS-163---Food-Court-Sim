2fb2b75610d3d6317d84003d6a6d40a75ad72d9e
deleonl <deleonl@mail.gvsu.edu>
2017-04-12 01:11:13 -0400

    Revert "Update FoodCourtLogic.java"
    This reverts commit c1714fb5b43f7b245d79882eeb5cc387fe707b34.

--- src/backEnd/FoodCourtLogic.java
+++ src/backEnd/FoodCourtLogic.java
@@ -1,39 +1,98 @@
 package backEnd;
 
-public class Cashier extends Eatery{
-	
-	private int totalTime;
-	
-	public void add(Person p) {
-		Q.enQ(p);
-		if (Q.size() > maxQlength)
-			maxQlength = Q.size();
-		p.setDestination(null);
+import java.util.ArrayList;
+
+public class FoodCourtLogic {
+	private Clock clk;
+	public ArrayList<Eatery> eateries;
+	private PersonProducer producer;
+	//private ArrayList<PersonProducer> producers;
+	private PayLine payLine;
+	private ArrayList<Cashier> checkOut;
+	private int quitTime;
+	private boolean end;
+
+	public FoodCourtLogic(int numTicksPerPerson, int averageEateryTime, int averageCashierTime, int quitTime) {
+		this.end = true;
+		clk = new Clock();
+		eateries = new ArrayList<Eatery>();
+
+		this.quitTime = quitTime;
+		checkOut = new ArrayList<Cashier>();
+		payLine = new PayLine(checkOut);
+		producer = new PersonProducer(eateries, numTicksPerPerson, averageEateryTime, averageCashierTime, payLine, quitTime);
+		//producers = new ArrayList<PersonProducer>();
+		clk.add(payLine);
+		clk.add(producer);
 	}
-	
-	public void event(int tick) throws EmptyQException{
-		System.out.println("Eatery Q: " + Q.size());
-		if (tick >= timeOfNextEvent) {
-			if (person != null) { 			// Notice the delay that takes place here
-				if(person.getDestination() != null) {
-					person.getDestination().add(person);
-				}    // take this person to the next station.
-				totalTime += person.getPersonalTime();
-				person = null;				// I have send the person on. 
-				isServicing = false;
-			}
-			
-			if (Q.size() >= 1) {
-				person = Q.deQ();		// do not send this person as of yet, make them wait.
-				isServicing = true;
-				timeOfNextEvent = tick + (int) (person.getCashierTime() + 1);
-				completed++;										
-			}	
+
+	public void addEatery() {
+		Eatery e = new Eatery();
+		//PersonProducer pp = new PersonProducer(e, 20, 18, 18, payLine);
+		eateries.add(e);
+		//clk.add(pp);
+		clk.add(e);
+	}
+	public int findMaxEateryLine(){
+		int m = 0;
+
+		for(int i = 0; i < eateries.size(); i++){
+			if(eateries.get(i).getMaxQlength() > m)
+				m = eateries.get(i).getMaxQlength();
 		}
-			System.out.println("Cashier Completed: " + completed);
+		return m;
 	}
-	
-	public double getAvgTime() {
-		return ((double) totalTime) / completed;
+	public int findPeopleLeft(){
+		int m = 0;
+
+		for(int i = 0; i < eateries.size(); i++){
+			m += eateries.get(i).getLeft();	
+		}
+		return m;
+	}
+	public double averageTime(){
+		double m = 0;
+		for(int i = 0; i < checkOut.size(); i++){
+			m += checkOut.get(i).getAvgTime();
+		}
+		return m / checkOut.size();
+	}
+	public void addCheckOut() {
+		Cashier c = new Cashier();
+		checkOut.add(c);
+		clk.add(c);
+	}
+
+	public void run(int endTime) throws EmptyQException {
+		clk.run(quitTime);
+		end = false;
+	}
+
+	public int getThroughput() {
+		int throughput = 0;
+		for(Eatery e: checkOut) {
+			throughput += e.getThroughPut();
+		}
+		return throughput;
+	}
+	public boolean getEnd(){
+		return end;
+	}
+	public static void main(String[] args) {
+		FoodCourtLogic fcl = new FoodCourtLogic(10, 30, 30, 1000000);
+		fcl.addCheckOut();
+		fcl.addCheckOut();
+		fcl.addEatery();
+		fcl.addEatery();
+		try {
+			fcl.run(100000);
+		}
+		catch(EmptyQException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Throughput: " + fcl.getThroughput());
+		System.out.println("Throughput Pay: " + fcl.payLine.getThroughPut());
+		System.out.println("Percent through check out 1: " + (double)fcl.checkOut.get(0).getThroughPut() / fcl.getThroughput());
 	}
 }
